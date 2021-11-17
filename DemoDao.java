64212ebf479a42dee593759731acbcaf187775e7
bhupathimalachi <bhupathim2018@gmail.com>
2021-11-17 21:30:37 +0530

    updated in DemoDao

--- DemoDao.java
+++ DemoDao.java
@@ -3,12 +3,34 @@ public class DemoDao{
 	public void  naturalNumbers(int x){
 		
 		int i=1;
+		System.out.println("Natural Numbers are:- ");
 		while(i<=x){
 			System.out.println(i);
 			i++;
 			
 		}
-		
+	}
+	public void  printEvenNumbers(int x){
+		int i=1;
+		System.out.println("even numbers  are:- ");
+		while(i<=x){
+			if(i%2 == 0)
+			{
+				System.out.println(i);
+			}
+			i++;
+		}
+	}
+	public void  printOddNumbers(int x){
+		int i=1;
+		System.out.println("odd numbers  are:- ");
+		while(i<=x){
+			if(i%2 == 1)
+			{
+				System.out.println(i);
+			}
+			i++;
+		}
 	}
 	
 	public static void main(String[] arg){
@@ -19,6 +41,7 @@ public class DemoDao{
 		System.out.println("enter N value to print:- ");
 		n=sc.nextInt();
 		dd.naturalNumbers(n);
-		
+		dd.printEvenNumbers(n);
+		dd.printOddNumbers(n);
 	}
 }
